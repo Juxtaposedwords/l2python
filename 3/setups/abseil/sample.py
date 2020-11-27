2bd9dffce979046ca68adbc959035c777fe30834
Maloy <juxtaposedwords@gmail.com>
2020-11-27 20:06:27 +0000

    formating with yapf

--- 3/setups/abseil/sample.py
+++ 3/setups/abseil/sample.py
@@ -3,5 +3,5 @@ def func(input: int) -> int:
     return 1 + input
 
 
-def divisor(input: int)-> int:
-    return 12/input
+def divisor(input: int) -> int:
+    return 12 / input
--- 3/setups/abseil/sample_test.py
+++ 3/setups/abseil/sample_test.py
@@ -1,44 +1,33 @@
 from absl.testing import absltest
 from absl.testing import parameterized
-from contextlib import contextmanager
 from absl import flags
 from copy import deepcopy
-from  sample import divisor
+from sample import divisor
 from contextlib import nullcontext
 
 
-
-
-@contextmanager
-def does_not_raise():
-    yield
-
 class UnitTests(parameterized.TestCase):
     @parameterized.named_parameters(
-       {
-           'testcase_name': 'Divided by zero',
-           'input' :0,
-           'want':{
-               'exception': ZeroDivisionError ,
-           },
-       },
-       {
-           'testcase_name': 'Happy path',
-           'input':12,
-           'want':{
-               'got':1,
-
-           },
-       })
-    def testDivisor(self,input: int,want:dict):
+        {
+            'testcase_name': 'Divided by zero',
+            'input': 0,
+            'want': {
+                'exception': ZeroDivisionError,
+            },
+        }, {
+            'testcase_name': 'Happy path',
+            'input': 12,
+            'want': {
+                'got': 1,
+            },
+        })
+    def testDivisor(self, input: int, want: dict):
         if 'exception' in want:
             with self.assertRaises(want['exception']):
                 divisor(input)
         else:
-            self.assertEqual(want['got'],divisor(input))
-
-
+            self.assertEqual(want['got'], divisor(input))
 
 
 if __name__ == '__main__':
-  absltest.main()
+    absltest.main()
