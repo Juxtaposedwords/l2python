d4504d05422373b3cd47589a2999e890a985f5cc
Maloy <juxtaposedwords@gmail.com>
2020-11-27 22:04:58 +0000

    makes more sense to put length on the right, i think?

--- 3/HackerRank/algorithms/chocolate_bar/choc.py
+++ 3/HackerRank/algorithms/chocolate_bar/choc.py
@@ -4,6 +4,6 @@ from typing import List
 
 # splitter attempts to break the list up into as equitable split as possible.
 def splitter(bar: List[int], groups: int) -> List[int]:
-    if len(bar) <= groups:
+    if groups > len(bar):
         raise ArithmeticError("%s is greater than the length of the bar (%d) provided"% (groups, len(bar)))
 
