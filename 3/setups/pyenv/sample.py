e27d46f79b4040bfe7ad371c06cc26ba0e763aab
Maloy <juxtaposedwords@gmail.com>
2020-11-27 20:02:57 +0000

    checking in pyenv samples

--- 3/setups/pyenv/sample.py
+++ 3/setups/pyenv/sample.py
@@ -1,4 +1,8 @@
 # content of test_sample.py
 def func(input: int) -> int:
-    return 6/ input
+    return 1 + input
+
+
+def divisor(input: int)-> int:
+    return 12/input
 
--- 3/setups/pyenv/sample_test.py
+++ 3/setups/pyenv/sample_test.py
@@ -6,17 +6,46 @@ import pytest
 @contextmanager
 def does_not_raise():
     yield
-
+# Copy/pasta from https://docs.pytest.org/en/stable/example/parametrize.html#paramexamples 
 @pytest.mark.parametrize(
-    "example_input,expectation",
+    "example_input,expectation,",
     [
         (3, does_not_raise()),
         (2, does_not_raise()),
         (1, does_not_raise()),
         (0, pytest.raises(ZeroDivisionError)),
-    ],
-)
-def test_answer(example_input, expectation):
+    ])
+def test_answer(example_input: int, expectation: Exception):
     """Test how much I know division."""
     with expectation:
         assert (6 / example_input) is not None
+
+
+@pytest.mark.parametrize(
+    "have,want",
+    [
+        (-1,0),
+        (2,3),
+        (3,4),
+    ],
+)
+def test_func(have: int,want:int):
+    assert want == sample.func(have)
+
+
+@pytest.fixture(params=[
+{
+    "input": 12,
+    "want": 1,
+},
+{
+    "input": 6,
+    "want":2,
+},
+{
+    "input":3,
+    "want":3,
+}
+])
+def test_divisor(test):
+    assert sample.divisor(test.input) == test.want
