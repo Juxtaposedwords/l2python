463d4ddde9b31b61caf2d6a2e38430c0adf3013d
Maloy <juxtaposedwords@gmail.com>
2020-11-27 21:25:55 +0000

    adding type annotations for dictionary

--- 3/setups/abseil/mocking/mocky.py
+++ 3/setups/abseil/mocking/mocky.py
@@ -9,6 +9,8 @@ class Example:
     def Get(self, input: str) -> int:
         return requests.get(input).status_code
 
+
 if __name__ == '__main__':
     x = Example()
-    print('Status code: %d' % x.Get('https://imgs.xkcd.com/comics/making_progress.png'))
+    print('Status code: %d' %
+          x.Get('https://imgs.xkcd.com/comics/making_progress.png'))
--- 3/setups/abseil/mocking/mocky_test.py
+++ 3/setups/abseil/mocking/mocky_test.py
@@ -2,26 +2,24 @@ from absl.testing import absltest
 from absl.testing import parameterized
 from mocky import Example
 from unittest import mock
+from typing import Any, Dict
 import requests
 
+
 class UnitTests(parameterized.TestCase):
-    @parameterized.named_parameters(
-        {
-            'testcase_name': 'happy path',
-            'get_resp': 301,
-            'want': {
-                'value': 301,
-            },
-        })
+    @parameterized.named_parameters({
+        'testcase_name': 'happy path',
+        'get_resp': 301,
+        'want': {
+            'value': 301,
+        },
+    })
     @mock.patch.object(requests, 'get')
-    def testDivisor(self, fake_get, get_resp: int,  want: dict):
+    def testDivisor(self, fake_get, get_resp: int, want: Dict[str, Any]):
         fake_session = mock.Mock()
         fake_session.status_code = get_resp
         fake_get.return_value = fake_session
-        self.assertEqual(want['value'],Example().Get(""))
-
-
-
+        self.assertEqual(want['value'], Example().Get(""))
 
 
 if __name__ == '__main__':
